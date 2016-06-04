e22cfa51b43889813340c0cbd8fe66342559571e
Pavel Stratil <pavel@killua.eu>
2016-06-04 16:53:53 +0000

    removing clutter

--- profiles/app/svc-lempstack/setup.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env bash
-layman -a mage-overlay
--- profiles/system/@common/setup.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-einfo "Emerging package sets"
-current=$(echo `eselect profile show` | sed -e 's/Current \/etc\/portage\/make.profile symlink: //g')
-eselect profile set default/linux/amd64/13.0/systemd
-${SCRIPT} tmerge -uDN @mage-adm-tools @mage-sys-core @mage-sys-fs @mage-sys-net @mage-sys-portage
-eselect profile set $current
